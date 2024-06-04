a1944d694534b7e0b70bbf60883dc64541ea9b1d
Rafael <r.munoz@aluno.ifsp.edu.br>
2024-06-04 09:09:33 -0300

    modulos

---
+++ 03-modulos-pacotes/main.py
@@ -0,0 +1,21 @@
+#importando tudo do módulo
+import matematica
+
+#importa elementos específicos
+from matematica import PI as PI_MAT, somar, subitrair
+#from matemática import *
+#from matemática import PI as PI_MAT, somar as somar_mat
+
+from estatistca.descritiva import media, maximo, minimo 
+from estatistca.inferencial import VALOR
+
+PI = 9999
+
+print(matematica.PI)
+
+print(matematica.somar(10.0,3.0))
+
+print(matematica.PI)
+print(somar(10.0,3))
+
+print(PI_MAT)
---
+++ 03-modulos-pacotes/matematica.py
@@ -0,0 +1,14 @@
+PI = 3.14
+
+# definição
+def somar(n1, n2):
+    return n1 + n2
+
+def subitrair(n1, n2):
+    return n1 - n2
+
+def multiplicar(n1, n2):
+    return n1 * n2
+
+def dividir(n1, n2):
+    return n1 + n2
